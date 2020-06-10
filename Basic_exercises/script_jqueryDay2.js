956d658751a3e459ca684ae62a465c7486057ee4
Carina Jurgovsky <carina.jurgovsky@gmx.at>
2020-06-10 13:57:22 +0200

    basic ex 1

---
+++ Basic_exercises/script_jqueryDay2.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+// 	$("#Santa_Clause").on("click", function() {
+// $("Santa_text").html("You clicked on Santa Clause")
+// 	});
+// });
+
+$(document).ready(function(){
+$("#Santa_Clause").click(function(){
+	$("#Santa_text").text("You clicked on Santa Clause")
+});
+})
