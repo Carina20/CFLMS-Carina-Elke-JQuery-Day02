d325427b0e14c8dfa397902c5f9b164934e0c997
Carina Jurgovsky <carina.jurgovsky@gmx.at>
2020-06-10 14:10:38 +0200

    update basic ex 1 with comments

--- Basic_exercises/script_jqueryDay2.js
+++ Basic_exercises/script_jqueryDay2.js
@@ -1,11 +1,24 @@
+
+/* --------------------this one DOESN'T work-----------------------------*/
+
 // $(document).ready(function(){
 // 	$("#Santa_Clause").on("click", function() {
 // $("Santa_text").html("You clicked on Santa Clause")
 // 	});
 // });
 
+/* --------------------this one works-----------------------------
+
+// $(document).ready(function(){
+// $("#Santa_Clause").click(function(){
+// 	$("#Santa_text").text("You clicked on Santa Clause")
+// });
+// })
+*/
+/* --------------------this one works too-----------------------------*/
+
 $(document).ready(function(){
-$("#Santa_Clause").click(function(){
-	$("#Santa_text").text("You clicked on Santa Clause")
-});
+	$("#Santa_Clause").click(function(){
+		$("#Santa_text").html("You clicked on Santa Clause")
+	});
 })
